eet formula fixed, earthquakes in profiles, coupled zones maps

rhe_params_yses.py
@@ -50,9 +50,9 @@ if __name__ == '__main__':
     lc_yield_temps_exact_dic = {}
     lm_yield_temps_exact_dic = {}
     for key, value in rhe_data.items():
-        if key in set(['6','22','29']):
+        #if key in set(['6','22','29']):
         #if key in set(map(str,np.arange(2,30,2))):
-        #if key:
+        if key:
             dys = model.mm.calc_ductile_yield_strength(m_input.e, value.n,
                 value.A, value.H, m_input.R, geotherm_1D)
             dys_list.append({'name': value.name, 'value': dys})
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     ax.set_title('YSEs')
     ax.plot(bys_1D, z_axis_2, 'k')
     colors = categorical_cmap(3, [len(uc_params),len(lc_params),len(lm_params)],
-        desaturated_first=False)(np.linspace(0,1,len(dys_list)))
+        desaturated_first=True)(np.linspace(0,1,len(dys_list)))
     colors_iterator = iter(colors)
     for i, dys in enumerate(dys_list):
         #print(dys)

src/plot.py
@@ -12,7 +12,7 @@ from mpl_toolkits.basemap import Basemap
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from src.utils import MidPointNorm, round_to_1, get_magnitude
 
-def base_latitude_profile(cs,gm,lat):
+def base_latitude_profile(cs,gm,lat,earthquakes=None, sli=None):
     # Axes configuration
     x_axis = cs.get_x_axis()
     z_axis = cs.get_z_axis()
@@ -31,13 +31,23 @@ def base_latitude_profile(cs,gm,lat):
     plt.plot(x_axis, gm.get_icd().cross_section(latitude=lat), 'w')
     plt.plot(x_axis, gm.get_moho().cross_section(latitude=lat), 'g')
     plt.plot(x_axis, gm.get_slab_lab().cross_section(latitude=lat), 'r')
+    if earthquakes is not None:
+        eq = earthquakes[
+            (earthquakes['Lat.'] >= lat - 0.1) &
+            (earthquakes['Lat.'] < lat + 0.1)]
+        plt.scatter(eq['Lon.'], -eq['Prof.'], color='k', s=eq['m1mag']**2.+20.,
+            zorder=1000)
+        print('lat:', lat, 'eq:', eq)
+    if sli is not None:
+        plt.scatter(sli['lon'], sli['depth'], color='r', zorder=1001, s=100)
     return fig
 
-def thermal_latitude_profile(tm, lat, save_dir=None, show=False, name='t'):
+def thermal_latitude_profile(tm, lat, save_dir=None, show=False, name='t',
+        earthquakes=None, sli=None):
     # Base
     cs = tm.get_coordinate_system()
     gm = tm.get_geometric_model()
-    fig = base_latitude_profile(cs, gm, lat)
+    fig = base_latitude_profile(cs, gm, lat, earthquakes=earthquakes, sli=sli)
     x_axis = cs.get_x_axis()
     z_axis = cs.get_z_axis()
     xx, zz = np.meshgrid(x_axis, z_axis)
@@ -54,20 +64,21 @@ def thermal_latitude_profile(tm, lat, save_dir=None, show=False, name='t'):
     numlabels=6
     contour = plt.contour(xx, zz, temps_masked.T, numlabels, colors='k')
     plt.clabel(contour, fontsize=9, fmt='%.0f', rightside_up=True)
-    plt.title('Perfil Termal %s' %(lat))
+    plt.title('Perfil Termal %.1f' %(lat))
     if show is True:
         plt.show()
     if save_dir:
-        name = name + '_%s' %(lat) + '.png'
+        name = name + '_%.1f' %(lat) + '.png'
         fig.savefig(save_dir + name)#, dpi='figure', format='pdf')
     plt.close()
     return
 
-def mechanic_latitude_profile(mm, lat, save_dir=None, show=False, name='m'):
+def mechanic_latitude_profile(mm, lat, save_dir=None, show=False, name='m',
+        earthquakes=None, sli=None):
     # Base
     cs = mm.get_coordinate_system()
     gm = mm.get_geometric_model()
-    fig = base_latitude_profile(cs, gm, lat)
+    fig = base_latitude_profile(cs, gm, lat, earthquakes=earthquakes, sli=sli)
     x_axis = cs.get_x_axis()
     z_axis = cs.get_z_axis()
     xx, zz = np.meshgrid(x_axis, z_axis)
@@ -80,11 +91,11 @@ def mechanic_latitude_profile(mm, lat, save_dir=None, show=False, name='m'):
     plt.clim(ys_min, ys_max)
     cbar = plt.colorbar(heatmap, aspect='20')
     cbar.set_label('Yield Strength', rotation=90, labelpad=-50)
-    plt.title('Perfil Mecanico %s' %(lat))
+    plt.title('Perfil Mecanico %.1f' %(lat))
     if show is True:
         plt.show()
     if save_dir:
-        name = name + '_%s' %(lat) + '.png'
+        name = name + '_%.1f' %(lat) + '.png'
         fig.savefig(save_dir + name)#, dpi='figure', format='pdf')
     plt.close()
     return
@@ -104,15 +115,28 @@ def base_map(topo=True):
     return map
 
 def boolean_map(
-        array_2D, color='green', map=None, ax=None, alpha=1, save_dir=None,
-        name='boolean_map', return_width_ratio=False, title=None):
+        array_2D, array_2D_2=None, color='green', map=None, ax=None,
+        alpha=0.6, save_dir=None, name='boolean_map', return_width_ratio=False,
+        title=None, cmap_idx=0):
     #Axes and map setup
     if ax is None:
         fig, ax = plt.subplots()
     if map is None:
         map = base_map(topo=False)
     #Imshow
-    map.imshow(array_2D.T, cmap=plt.cm.copper, origin='upper')
+    map.drawlsmask(land_color='0.0', ocean_color='0.0', resolution='l')
+    """
+    cmap = plt.cm.copper
+    cmap_colors = cmap(np.arange(cmap.N))
+    cmap_colors[:, -1] = np.linspace(0, 1, cmap.N)
+    cmap = colors.ListedColormap(cmap_colors)
+    """
+    cmap1 = colors.ListedColormap([[1,1,1,0],[0.75,0.75,0,0.5]])
+    cmap2 = colors.ListedColormap([[1,1,1,0],[0,0.75,0.75,0.5]])
+    cmaps = [cmap1,cmap2]
+    map.imshow(array_2D.T, cmap=cmaps[cmap_idx], origin='upper')
+    if array_2D_2 is not None:
+        map.imshow(array_2D_2.T, cmap=cmap2, origin='upper')#, alpha=alpha)
     # Title
     if title is not None:
         ax.set_title(title)

termomecanico.py
@@ -1,13 +1,16 @@
 import numpy as np
 from glob import glob
 from src.setup import data_setup, input_setup, exec_setup
-from src.compute import compute
+from src.compute import compute, SpatialArray2D
 from src.rmse import rmse
 from src.plot import (thermal_latitude_profile, mechanic_latitude_profile,
                    heatmap_map, data_map, diff_map, multi_map, data_scatter_plot)
 from src.datos_q import shf_data, shf_data_coords, shf_data_types, shf_data_error
 from src.utils import makedir
 from src.colormaps import jet_white_r
+from pandas import read_excel
+eq1 = read_excel("earthquakes/CSN.xlsx", sheet_name="Sheet1")
+eq2 = read_excel("earthquakes/CSN.xlsx", sheet_name="Sheet2")
 
 
 def termomecanico(t_input, m_input):
@@ -92,12 +95,20 @@ if __name__ == '__main__':
                 ishf_models, ishf_labels, save_dir=scatter_dir)
 
     #Latitude profiles
-    for lat in np.arange(exec_input.tmi, exec_input.tmx, -exec_input.tdelta):
+    sli_idx_array = model.gm.slab_lab_int_index
+    sli_lon_array = model.cs.get_x_axis()[sli_idx_array]
+    sli_depth_array = model.gm.slab_lab_int_depth
+    #for lat in np.arange(exec_input.tmi, exec_input.tmx, -exec_input.tdelta):
+    for idx, lat in enumerate(model.cs.get_y_axis()[:-1]):
+        sli_lon = sli_lon_array[idx]
+        sli_depth = sli_depth_array[idx]
         if exec_input.xt2:
             save_dir = direTer + 'Perfiles/'
             makedir(save_dir)
-            thermal_latitude_profile(model.tm, lat, save_dir, name='t')
+            thermal_latitude_profile(model.tm, lat, save_dir, name='t',
+                earthquakes=eq1, sli={'lon': sli_lon, 'depth': sli_depth})
         if exec_input.xm2:
             save_dir = direMec + 'Perfiles/'
             makedir(save_dir)
-            mechanic_latitude_profile(model.mm, lat, save_dir, name='m')
+            mechanic_latitude_profile(model.mm, lat, save_dir, name='m',
+                earthquakes=eq1, sli={'lon': sli_lon, 'depth': sli_depth})